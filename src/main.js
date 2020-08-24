9e986e6062cb2bee8bc0f738317af1fe7af27e5a
niezhichao <727841296@qq.com>
2020-08-24 17:31:18 +0800

    init proj

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'font-awesome/css/font-awesome.min.css'
+import './assets/theme/index.css'
+import Element from 'element-ui'
+
+Vue.config.productionTip = false
+
+Vue.use(Element);
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
---
+++ src/views/layout/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="app-wrapper">
+		<sidebar class="sidebar-container" :items="items"></sidebar>
+	</div>
+</template>
+
+<script>
+	import {Sidebar} from "./components";
+	
+	export default {
+		name:"Layout",
+		components:{
+			Sidebar
+		},
+		data(){
+			return {
+				items:[]
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
